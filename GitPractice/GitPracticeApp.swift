fa156ce2b6fd5c6434ef2b8bdab7d8e1373566be
Joan <joanm95@gmail.com>
2025-04-06 17:46:03 -0400

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by joan on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
